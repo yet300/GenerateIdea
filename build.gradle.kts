79e94196bc2cd9ffd60beb9c6fa8be1276adb702
yet300 <ghg00rus300@gmail.com>
2025-10-19 01:52:37 +0400

    ``` build: Add KSP, Serialization, and SQLDelight plugins
    Adds the KSP, Kotlin Serialization, and SQLDelight Gradle plugins to the root `build.gradle.kts` file. This makes them available for use across modules in the project.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,7 @@ plugins {
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.sqlDelight) apply false
 }
